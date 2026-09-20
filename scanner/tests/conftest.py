6d34fcc630c7799016370d593bad6f5114ede533
Annafi <islamannafi@gmail.com>
2026-09-20 18:20:01 -0400

    Fix: Made sure all the tests are running successfully

--- scanner/tests/conftest.py
+++ scanner/tests/conftest.py
@@ -1,16 +1,21 @@
 import pytest
-from sqlalchemy.orm import Session
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
-from api.db.session import engine
-from api.main import app, get_session
+from api.auth import get_current_user
+from api.db.session import engine, get_session
+from api.db.models import User
+from api.main import app
 
 
 @pytest.fixture
 def session():
     connection = engine.connect()
     transaction = connection.begin()
-    session = Session(bind=connection, join_transaction_mode="create_savepoint")
+    session = Session(
+        bind=connection,
+        join_transaction_mode="create_savepoint",
+    )
 
     try:
         yield session
@@ -22,7 +27,15 @@ def session():
 
 @pytest.fixture
 def client(session):
+    test_user = User(
+        email="test@example.com",
+        password_hash="",
+    )
+
     app.dependency_overrides[get_session] = lambda: session
+    app.dependency_overrides[get_current_user] = lambda: test_user
+
     with TestClient(app) as c:
         yield c
+
     app.dependency_overrides.clear()
